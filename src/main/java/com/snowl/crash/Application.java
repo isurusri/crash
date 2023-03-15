3feb2d1c335e9520d09c6e34cd08083771e50568
Isuru Srimal <isuru.srimal258@gmail.com>
2023-03-15 23:36:50 +0530

    init

---
+++ src/main/java/com/snowl/crash/Application.java
@@ -0,0 +1,14 @@
+package com.snowl.crash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.ConfigurableApplicationContext;import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    }
+
+}
